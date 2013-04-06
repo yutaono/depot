f73154e8a1f844ec2d7767e33dc612ea47231d64
yutaono <vrf.ced@gmail.com>
2013-04-06 15:26:09 +0900

    Display Catalog

---
+++ test/functional/store_controller_test.rb
@@ -0,0 +1,13 @@
+require 'test_helper'
+
+class StoreControllerTest < ActionController::TestCase
+  test "should get index" do
+    get :index
+    assert_response :success
+    assert_select '#columns #side a', minimum: 2
+    assert_select '#main .entry', 3
+    assert_select 'h3', 'Programming Ruby 1.9r'
+    assert_select '.price', /\$[,\d]+\.\d\d/
+  end
+
+end
